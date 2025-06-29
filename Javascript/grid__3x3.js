323778692baf093d6c420c9b0b094d0200eb858b
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-06-29 14:58:03 +0500

    Grid_3x3 script codes added and updated2 06/29/2025

--- Javascript/grid__3x3.js
+++ Javascript/grid__3x3.js
@@ -1,3 +1,30 @@
+
+const status = document.getElementById('status');
 const restartGame = document.getElementById('restart__Game');
 const cells = document.querySelectorAll('.cell');
-const status = document.getElementById('status');
+
+let game_Board = ["", "", "", "", "", "", "", "", ""];
+let game_Active = true;
+let current_Player = 'X';
+
+const winning_Patterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+
+//Let's create a checkWinner() function for winning patterns:)
+
+function checkWinner() {
+    for (const combo of winning_Patterns) {
+        const [a, b, c] = combo;
+        if (game_Board[a] && game_Board[a] === game_Board[b] && game_Board[b] === game_Board[c]) {
+            status.textContent = `Player ${game_Board[a]} Wins 😎!`;
+            game_Active = false;
+            return;
+        }
+    }
+    if(!game_Board.includes('')){
+        status.textContent = "It's Draw 😟!";
+    }
+}
